1de74ad4768d65c96a1534ee257fbcfc3aea069e
abe.yasuhiro <aya.0058@gmail.com>
2015-03-26 20:17:59 +0900

    lesson 1 終了！

--- app/webroot/js/app.js
+++ app/webroot/js/app.js
@@ -2,6 +2,12 @@ var app = app || {};
 
 //開始
 (function(app) {
-	var todoRouter = new app.TodoRouter();
-	Backbone.history.start();
+	app.Application = Backbone.Marionette.Application.extend({
+		initialize : function(){
+			new app.TodoRouter();
+		},
+		onStart : function(){
+			Backbone.history.start();
+		},
+	});
 })(app);
---
+++ app/webroot/js/main.js
@@ -0,0 +1,6 @@
+var app = app || {};
+//開始
+(function(app) {
+	app.application = new app.Application();
+	app.application.start();
+})(app);
